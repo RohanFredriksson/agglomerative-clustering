75e06636057d8bcdc4051f8d7c5108c5e4d83036
Rohan Fredriksson <rohan.fredriksson@gmail.com>
2025-06-01 15:04:35 +1000

    Moved all of the data structures into a single header file.

--- src/clustering.cpp
+++ src/clustering.cpp
@@ -1,17 +1,12 @@
 // Average Hierarchical Agglomerative Color Clustering
-#include <map>
-#include <vector>
-#include <cstdint>
-#include <cstdlib>
-#include <cstring>
-#include <algorithm>
+#include "clustering.hpp"
 #include <emscripten.h>
-#include <unordered_map>
-#include <unordered_set>
 
 inline void hash_combine(std::size_t& seed, std::size_t value) {
     seed ^= value + 0x9e3779b9 + (seed << 6) + (seed >> 2);
 }
+
+#include <iostream>
 
 class Color {
 
@@ -48,296 +43,6 @@ namespace std {
     };
 }
 
-class Vector3 {
-    
-public:
-
-    uint16_t x;
-    uint16_t y;
-    uint16_t z;
-
-    Vector3() {
-        this->x = 0u;
-        this->y = 0u;
-        this->z = 0u;
-    }
-
-    Vector3(uint16_t x, uint16_t y, uint16_t z) {
-        this->x = x;
-        this->y = y;
-        this->z = z;
-    }
-
-    bool operator==(const Vector3& other) const {
-        return (x == other.x) &&
-               (y == other.y) &&
-               (z == other.z);
-    }
-
-};
-
-namespace std {
-    template <>
-    struct hash<Vector3> {
-        std::size_t operator()(const Vector3& v) const noexcept {
-            std::size_t seed = 0;
-            hash_combine(seed, v.x);
-            hash_combine(seed, v.y);
-            hash_combine(seed, v.z);
-            return seed;
-        }
-    };
-}
-
-class Pair {
-
-public:
-
-    Vector3 a;
-    Vector3 b;
-    uint64_t distance = std::numeric_limits<uint64_t>::max();
-
-    Pair() {
-
-    }
-
-    Pair(Vector3 a, Vector3 b) {
-    
-        this->a = a;
-        this->b = b;
-
-        uint64_t dx = a.x > b.x ? (uint64_t)(a.x - b.x) : (uint64_t)(b.x - a.x);
-        uint64_t dy = a.y > b.y ? (uint64_t)(a.y - b.y) : (uint64_t)(b.y - a.y);
-        uint64_t dz = a.z > b.z ? (uint64_t)(a.z - b.z) : (uint64_t)(b.z - a.z);
-
-        this->distance = dx * dx + dy * dy + dz * dz;
-
-    }
-
-    bool operator>=(const Pair& other) const {
-        return distance >= other.distance;
-    }
-
-    bool contains(Vector3 vector) {
-        return this->a == vector || this->b == vector;
-    }
-
-};
-
-class Bucket {
-
-public:
-
-    Vector3 location;
-    std::unordered_set<Vector3> points;
-    Pair best;
-
-    Bucket(Vector3 location) : location(location) {
-
-    }
-
-};
-
-class CoarseningGrid {
-
-private:
-
-    uint16_t resolution;
-    uint16_t grid_size;
-    std::unordered_map<Vector3, Bucket*> grid;
-    std::map<uint64_t, std::unordered_set<Bucket*>> cache;
-
-    std::vector<Bucket*> get_local_buckets(Vector3 location) {
-
-        std::vector<Bucket*> result;
-        result.reserve(27);
-
-        Vector3 min = location;
-        if (min.x > 0u) {min.x--;}
-        if (min.y > 0u) {min.y--;}
-        if (min.z > 0u) {min.z--;}
-
-        Vector3 max = location;
-        max.x++;
-        max.y++;
-        max.z++;
-
-        for (uint16_t x = min.x; x <= max.x; x++) {
-            for (uint16_t y = min.y; y <= max.y; y++) {
-                for (uint16_t z = min.z; z <= max.z; z++) {
-                    Vector3 bucket_position = Vector3(x, y, z);
-                    if (this->grid.find(bucket_position) == this->grid.end()) {continue;}
-                    result.push_back(this->grid[bucket_position]);
-                }
-            }
-        }
-
-        return result;
-
-    }
-
-    std::vector<Vector3> get_all_points() {
-        
-        size_t total_size = 0;
-        std::vector<Vector3> result;
-        for (const auto& pair : this->grid) {total_size += pair.second->points.size();}
-        result.reserve(total_size);
-
-        for (const auto& pair : this->grid) {result.insert(result.end(), pair.second->points.begin(), pair.second->points.end());}
-        return result;
-
-    }
-
-    std::vector<Vector3> get_local_points(Vector3 location) {
-
-        size_t total_size = 0; 
-        std::vector<Bucket*> buckets = this->get_local_buckets(location);
-        for (Bucket* bucket : buckets) {total_size += bucket->points.size();}
-        
-        std::vector<Vector3> result;
-        result.reserve(total_size);
-        for (Bucket* bucket : buckets) {result.insert(result.end(), bucket->points.begin(), bucket->points.end());}
-        return result;
-
-    }
-
-    Vector3 get_location(Vector3 point) {
-        return Vector3(
-            point.x / this->grid_size,
-            point.y / this->grid_size,
-            point.z / this->grid_size
-        );   
-    }
-
-    void add_cache(Bucket* bucket) {
-        if (this->cache.find(bucket->best.distance) == this->cache.end()) {this->cache[bucket->best.distance] = std::unordered_set<Bucket*>();}
-        this->cache[bucket->best.distance].insert(bucket);
-    } 
-
-    void remove_cache(Bucket* bucket) {
-        this->cache[bucket->best.distance].erase(bucket);
-        if (this->cache[bucket->best.distance].size() == 0) {this->cache.erase(bucket->best.distance);}
-    }
-
-public:
-
-    CoarseningGrid(uint16_t resolution) {
-        this->resolution = resolution;
-        this->grid_size = std::numeric_limits<uint16_t>::max() / (1u << this->resolution);
-    }
-
-    ~CoarseningGrid() {
-        for (const auto& pair : this->grid) {
-            delete pair.second;
-        }
-    }
-
-    void add(Vector3 point) {
-
-        // If we don't have the required bucket, allocate one.
-        Vector3 location = this->get_location(point);
-        if (this->grid.find(location) == this->grid.end()) {
-            Bucket* bucket = new Bucket(location);
-            this->grid[location] = bucket;
-            this->add_cache(bucket);
-        }
-
-        // If we already have this point, don't add it.
-        else if (this->grid[location]->points.find(point) != this->grid[location]->points.end()) {
-            return;
-        }
-
-        std::vector<Bucket*> buckets = this->get_local_buckets(location);
-        for (Bucket* bucket : buckets) {
-
-            bool recache = false;
-            Pair best = bucket->best;
-
-            for (const Vector3& current_point : bucket->points) {
-                Pair pair(current_point, point);
-                if (pair >= best) {continue;}
-                recache = true;
-                best = pair;
-            }
-
-            if (!recache) {continue;}
-            this->remove_cache(bucket);
-            bucket->best = best;
-            this->add_cache(bucket);
-
-        }
-
-        // Insert into the bucket
-        this->grid[location]->points.insert(point);
-        
-    }
-
-    void remove(Vector3 point) {
-
-        // We can't remove a point if there is no bucket for it.
-        Vector3 location = this->get_location(point);
-        if (this->grid.find(location) == this->grid.end()) {return;}
-        Bucket* bucket = this->grid[location];
-
-        // If the point isn't in the bucket, we can't remove it.
-        if (bucket->points.find(point) == bucket->points.end()) {return;}
-        this->grid[location]->points.erase(point);
-
-        // If the bucket is empty delete the bucket.
-        if (bucket->points.size() == 0) {
-            this->grid.erase(location);
-            this->remove_cache(bucket);
-            delete bucket;
-        }
-
-        std::vector<Bucket*> buckets = this->get_local_buckets(location);
-        for (Bucket* bucket : buckets) {
-
-            // If the best point doesn't include the removed point we don't need to recache.
-            if (!bucket->best.contains(point)) {continue;}
-            this->remove_cache(bucket);
-            bucket->best = Pair();
-
-            // Brute force closest point search, this should be okay since there should not be too many points.
-            std::vector<Vector3> local_points = this->get_local_points(bucket->location);
-            for (size_t i = 0; i < local_points.size(); i++) {
-                for (const Vector3& current_point : bucket->points) {
-                    if (current_point == local_points[i]) {continue;}
-                    Pair pair(current_point, local_points[i]);
-                    if (pair >= bucket->best) {continue;}
-                    bucket->best = pair;
-                }
-            }
-
-            this->add_cache(bucket);
-
-        }
-
-    }
-    
-    Pair get_nearest() {
-    
-        if (this->cache.size() == 0) {return Pair();}
-        std::unordered_set<Bucket*> best_buckets = this->cache.begin()->second;
-
-        Pair best = (*best_buckets.begin())->best;
-        if (best.distance != std::numeric_limits<uint64_t>::max()) {return best;}
-        if (best.distance == std::numeric_limits<uint64_t>::max() && this->resolution == 0u) {return Pair();}
-
-        std::vector<Vector3> points = this->get_all_points();
-        for (const auto& pair : this->grid) {delete pair.second;}
-        this->cache.clear();
-        this->grid.clear();
-
-        this->resolution = this->resolution - 1u;
-        this->grid_size = std::numeric_limits<uint16_t>::max() / (1u << this->resolution);
-        for (Vector3 point : points) {this->add(point);}
-        return this->get_nearest();
-
-    }
-
-};
-
 inline uint16_t uint8_to_uint16(uint8_t value) {
     return (uint16_t(value) * std::numeric_limits<uint16_t>::max()) / std::numeric_limits<uint8_t>::max();
 }
@@ -357,27 +62,20 @@ uint8_t* pack_variable_size(std::vector<uint8_t> vector) {
 }
 
 std::vector<uint8_t> _get_clustering(uint8_t* image_data, int image_length, int image_format) {
-    
-    std::vector<uint8_t> clustering;
-    std::unordered_map<Vector3, uint32_t> histogram;
-    CoarseningGrid coarsening_grid(5u);
 
-    int limit = image_length - 2;
-    int increment = 3;
+    std::vector<uint8_t> clustering;
+    AgglomerativeClustering::CoarseningGrid grid(5u);
+    AgglomerativeClustering::AgglomerativeHistogram histogram;
+
+    int limit = image_format == 1 ? image_length - 2 : image_length - 3;
+    int increment = image_format == 1 ? 3 : 4;
 
     for (int i = 0; i < limit; i += increment) {
-
-        Vector3 color(uint8_to_uint16(image_data[i]), uint8_to_uint16(image_data[i+1]), uint8_to_uint16(image_data[i+2]));
-        
-        // If we already have this point, increment the histogram
-        if (histogram.find(color) != histogram.end()) {
-            histogram[color] = histogram[color] + 1u;
-            continue;
+        std::array<uint16_t, 3> color = {uint8_to_uint16(image_data[i]), uint8_to_uint16(image_data[i+1]), uint8_to_uint16(image_data[i+2])};
+        if (histogram.count(color)) {
+            grid.add(color);
         }
-
-        coarsening_grid.add(color);
-        histogram[color] = 1u;
-
+        
     }
 
     size_t clustering_size = 3 + 9 * (histogram.size() - 1);
@@ -386,47 +84,34 @@ std::vector<uint8_t> _get_clustering(uint8_t* image_data, int image_length, int 
 
     for (int i = histogram.size() - 2; i >= 0; i--) {
 
-        Pair best_pair = coarsening_grid.get_nearest();
-        if (best_pair.distance == std::numeric_limits<uint64_t>::max()) {break;}
-
-        // Merge the nearest points together.
-        uint32_t a_count = histogram[best_pair.a];
-        uint32_t b_count = histogram[best_pair.b];
-        uint32_t merged_count = a_count + b_count;
-        double a_ratio = (double) a_count / (double) merged_count;
-        double b_ratio = (double) b_count / (double) merged_count;
-        Vector3 merged(best_pair.a.x * a_ratio + best_pair.b.x * b_ratio, best_pair.a.y * a_ratio + best_pair.b.y * b_ratio, best_pair.a.z * a_ratio + best_pair.b.z * b_ratio);
-
-        // Update the histogram
-        histogram.erase(best_pair.a);
-        histogram.erase(best_pair.b);
-        if (histogram.find(merged) == histogram.end()) {histogram[merged] = merged_count;}
-        else {histogram[merged] = histogram[merged] + merged_count;}
-
-        // Update the grid
-        coarsening_grid.remove(best_pair.a);
-        coarsening_grid.remove(best_pair.b);
-        coarsening_grid.add(merged);
-
+        std::optional<std::pair<std::array<uint16_t, 3>, std::array<uint16_t, 3>>> result = grid.get_nearest();
+        if (!result.has_value()) {break;}
+        
+        auto [a, b] = result.value();
+        std::array<uint16_t, 3> m = histogram.merge(a, b);
+        grid.remove(a);
+        grid.remove(b);
+        grid.add(m);
+        
         // Add the clustering operation
         size_t offset = 3 + 9 * i;
-        clustering[offset] = uint16_to_uint8(merged.x);
-        clustering[offset + 1] = uint16_to_uint8(merged.y);
-        clustering[offset + 2] = uint16_to_uint8(merged.z);
-        clustering[offset + 3] = uint16_to_uint8(best_pair.a.x);
-        clustering[offset + 4] = uint16_to_uint8(best_pair.a.y);
-        clustering[offset + 5] = uint16_to_uint8(best_pair.a.z);
-        clustering[offset + 6] = uint16_to_uint8(best_pair.b.x);
-        clustering[offset + 7] = uint16_to_uint8(best_pair.b.y);
-        clustering[offset + 8] = uint16_to_uint8(best_pair.b.z);
+        clustering[offset + 0] = uint16_to_uint8(m[0]);
+        clustering[offset + 1] = uint16_to_uint8(m[1]);
+        clustering[offset + 2] = uint16_to_uint8(m[2]);
+        clustering[offset + 3] = uint16_to_uint8(a[0]);
+        clustering[offset + 4] = uint16_to_uint8(a[1]);
+        clustering[offset + 5] = uint16_to_uint8(a[2]);
+        clustering[offset + 6] = uint16_to_uint8(b[0]);
+        clustering[offset + 7] = uint16_to_uint8(b[1]);
+        clustering[offset + 8] = uint16_to_uint8(b[2]);
 
     }
 
     // Add the k=1 clustering colour
-    Vector3 last = histogram.begin()->first;
-    clustering[0] = uint16_to_uint8(last.x);
-    clustering[1] = uint16_to_uint8(last.y);
-    clustering[2] = uint16_to_uint8(last.z);
+    std::array<uint16_t, 3> last = histogram.last();
+    clustering[0] = uint16_to_uint8(last[0]);
+    clustering[1] = uint16_to_uint8(last[1]);
+    clustering[2] = uint16_to_uint8(last[2]);
     return clustering;
 
 }
@@ -471,17 +156,17 @@ uint8_t* get_clustering(uint8_t* image_data, int image_length, int image_format)
 }
 
 EMSCRIPTEN_KEEPALIVE
-uint8_t* get_palette_from_clustering(uint8_t* clustering_data, int clustering_length, int k) {
-    std::vector<uint8_t> clustering;
-    clustering.resize(clustering_length);
-    std::memcpy(clustering.data(), clustering_data, clustering_length);
+uint8_t* get_palette(uint8_t* image_data, int image_length, int image_format, int k) {
+    std::vector<uint8_t> clustering = _get_clustering(image_data, image_length, image_format);
     std::vector<uint8_t> palette = _get_palette_from_clustering(clustering, k);
     return pack_variable_size(palette);
 }
 
 EMSCRIPTEN_KEEPALIVE
-uint8_t* get_palette(uint8_t* image_data, int image_length, int image_format, int k) {
-    std::vector<uint8_t> clustering = _get_clustering(image_data, image_length, image_format);
+uint8_t* get_palette_from_clustering(uint8_t* clustering_data, int clustering_length, int k) {
+    std::vector<uint8_t> clustering;
+    clustering.resize(clustering_length);
+    std::memcpy(clustering.data(), clustering_data, clustering_length);
     std::vector<uint8_t> palette = _get_palette_from_clustering(clustering, k);
     return pack_variable_size(palette);
 }
---
+++ src/clustering.hpp
@@ -0,0 +1,332 @@
+#if __cplusplus < 201703L
+#error ImageProcessing requires at least C++17"
+#endif
+
+#ifndef IMAGE_PROCESSING_HPP
+#define IMAGE_PROCESSING_HPP
+
+#include <unordered_map>
+#include <unordered_set>
+#include <functional>
+#include <stdexcept>
+#include <optional>
+#include <cstdint>
+#include <utility>
+#include <limits>
+#include <array>
+#include <map>
+
+namespace AgglomerativeClustering {
+
+class CoarseningGrid {
+
+private:
+
+    struct ArrayHash {
+        std::size_t operator()(const std::array<uint16_t, 3>& arr) const noexcept {
+            size_t seed = 0;
+            for (auto elem : arr) {seed ^= std::hash<uint16_t>{}(elem) + 0x9e3779b9 + (seed << 6) + (seed >> 2);}
+            return seed;
+        }
+    };
+
+    struct Pair {
+
+        std::array<uint16_t, 3> a{};
+        std::array<uint16_t, 3> b{};
+        uint64_t distance = std::numeric_limits<uint64_t>::max();
+
+        Pair() = default;
+
+        Pair(const std::array<uint16_t, 3>& a, const std::array<uint16_t, 3>& b) : a(a), b(b) {
+            uint64_t dx = static_cast<uint64_t>(a[0] > b[0] ? a[0] - b[0] : b[0] - a[0]);
+            uint64_t dy = static_cast<uint64_t>(a[1] > b[1] ? a[1] - b[1] : b[1] - a[1]);
+            uint64_t dz = static_cast<uint64_t>(a[2] > b[2] ? a[2] - b[2] : b[2] - a[2]);
+            distance = dx * dx + dy * dy + dz * dz;
+        }
+
+        bool operator>=(const Pair& other) const {
+            return distance >= other.distance;
+        }
+
+        bool contains(const std::array<uint16_t, 3>& point) const {
+            return a == point || b == point;
+        }
+
+    };
+
+    struct Bucket {
+
+        std::array<uint16_t, 3> location{};
+        std::unordered_set<std::array<uint16_t, 3>, ArrayHash> points;
+        Pair best;
+
+        Bucket(const std::array<uint16_t, 3>& location) : location(location) {}
+
+    };
+
+    uint16_t resolution;
+    uint16_t grid_size;
+    std::unordered_map<std::array<uint16_t, 3>, Bucket*, ArrayHash> grid;
+    std::map<uint64_t, std::unordered_set<Bucket*>> cache;
+
+    std::vector<Bucket*> get_local_buckets(const std::array<uint16_t, 3>& location) const {
+
+        std::vector<Bucket*> result;
+        result.reserve(27);
+
+        std::array<uint16_t, 3> min = location;
+        if (min[0] > 0u) {min[0]--;}
+        if (min[1] > 0u) {min[1]--;}
+        if (min[2] > 0u) {min[2]--;}
+
+        std::array<uint16_t, 3> max = location;
+        max[0]++;
+        max[1]++;
+        max[2]++;
+
+        for (uint16_t x = min[0]; x <= max[0]; x++) {
+            for (uint16_t y = min[1]; y <= max[1]; y++) {
+                for (uint16_t z = min[2]; z <= max[2]; z++) {
+                    std::array<uint16_t, 3> bucket_position = {x, y, z};
+                    auto it = this->grid.find(bucket_position);
+                    if (it != this->grid.end()) {result.push_back(it->second);}
+                }
+            }
+        }
+
+        return result;
+
+    }
+
+    std::vector<std::array<uint16_t, 3>> get_all_points() const {
+        
+        size_t total_size = 0;
+        std::vector<std::array<uint16_t, 3>> result;
+        for (const auto& pair : this->grid) {total_size += pair.second->points.size();}
+        result.reserve(total_size);
+
+        for (const auto& pair : this->grid) {result.insert(result.end(), pair.second->points.begin(), pair.second->points.end());}
+        return result;
+
+    }
+
+    std::vector<std::array<uint16_t, 3>> get_local_points(std::array<uint16_t, 3> location) const {
+
+        size_t total_size = 0; 
+        std::vector<Bucket*> buckets = this->get_local_buckets(location);
+        for (Bucket* bucket : buckets) {total_size += bucket->points.size();}
+        
+        std::vector<std::array<uint16_t, 3>> result;
+        result.reserve(total_size);
+        for (Bucket* bucket : buckets) {result.insert(result.end(), bucket->points.begin(), bucket->points.end());}
+        return result;
+
+    }
+
+    std::array<uint16_t, 3> get_location(const std::array<uint16_t, 3>& point) const {
+        std::array<uint16_t, 3> result = {static_cast<uint16_t>(point[0] / this->grid_size), static_cast<uint16_t>(point[1] / this->grid_size), static_cast<uint16_t>(point[2] / this->grid_size)};
+        return result;
+    }
+
+    void add_cache(Bucket* bucket) {
+        if (this->cache.find(bucket->best.distance) == this->cache.end()) {this->cache[bucket->best.distance] = std::unordered_set<Bucket*>();}
+        this->cache[bucket->best.distance].insert(bucket);
+    } 
+
+    void remove_cache(Bucket* bucket) {
+        this->cache[bucket->best.distance].erase(bucket);
+        if (this->cache[bucket->best.distance].size() == 0) {this->cache.erase(bucket->best.distance);}
+    }
+
+public:
+
+    CoarseningGrid(uint16_t resolution) {
+        this->resolution = resolution;
+        this->grid_size = std::numeric_limits<uint16_t>::max() / (1u << this->resolution);
+    }
+
+    ~CoarseningGrid() {
+        for (const auto& pair : this->grid) {
+            delete pair.second;
+        }
+    }
+
+    inline void add(uint16_t x, uint16_t y, uint16_t z) {
+        std::array<uint16_t, 3> point = {x, y, z};
+        this->add(point);
+    }
+
+    void add(std::array<uint16_t, 3> point) {
+
+        // If we don't have the required bucket, allocate one.
+        std::array<uint16_t, 3> location = this->get_location(point);
+        if (this->grid.find(location) == this->grid.end()) {
+            Bucket* bucket = new Bucket(location);
+            this->grid[location] = bucket;
+            this->add_cache(bucket);
+        }
+
+        // If we already have this point, don't add it.
+        else if (this->grid[location]->points.find(point) != this->grid[location]->points.end()) {
+            return;
+        }
+
+        std::vector<Bucket*> buckets = this->get_local_buckets(location);
+        for (Bucket* bucket : buckets) {
+
+            bool recache = false;
+            Pair best = bucket->best;
+
+            for (const std::array<uint16_t, 3>& current_point : bucket->points) {
+                Pair pair(current_point, point);
+                if (pair >= best) {continue;}
+                recache = true;
+                best = pair;
+            }
+
+            if (!recache) {continue;}
+            this->remove_cache(bucket);
+            bucket->best = best;
+            this->add_cache(bucket);
+
+        }
+
+        // Insert into the bucket
+        this->grid[location]->points.insert(point);
+        
+    }
+
+    inline void remove(uint16_t x, uint16_t y, uint16_t z) {
+        std::array<uint16_t, 3> point = {x, y, z};
+        this->remove(point);
+    }
+
+    void remove(std::array<uint16_t, 3> point) {
+
+        // We can't remove a point if there is no bucket for it.
+        std::array<uint16_t, 3> location = this->get_location(point);
+        if (this->grid.find(location) == this->grid.end()) {return;}
+        Bucket* bucket = this->grid[location];
+
+        // If the point isn't in the bucket, we can't remove it.
+        if (bucket->points.find(point) == bucket->points.end()) {return;}
+        this->grid[location]->points.erase(point);
+
+        // If the bucket is empty delete the bucket.
+        if (bucket->points.size() == 0) {
+            this->grid.erase(location);
+            this->remove_cache(bucket);
+            delete bucket;
+        }
+
+        std::vector<Bucket*> buckets = this->get_local_buckets(location);
+        for (Bucket* bucket : buckets) {
+
+            // If the best point doesn't include the removed point we don't need to recache.
+            if (!bucket->best.contains(point)) {continue;}
+            this->remove_cache(bucket);
+            bucket->best = Pair();
+
+            // Brute force closest point search, this should be okay since there should not be too many points.
+            std::vector<std::array<uint16_t, 3>> local_points = this->get_local_points(bucket->location);
+            for (size_t i = 0; i < local_points.size(); i++) {
+                for (const std::array<uint16_t, 3>& current_point : bucket->points) {
+                    if (current_point == local_points[i]) {continue;}
+                    Pair pair(current_point, local_points[i]);
+                    if (pair >= bucket->best) {continue;}
+                    bucket->best = pair;
+                }
+            }
+
+            this->add_cache(bucket);
+
+        }
+
+    }
+    
+    [[nodiscard]] std::optional<std::pair<std::array<uint16_t, 3>, std::array<uint16_t, 3>>> get_nearest() {
+    
+        if (this->cache.size() == 0) {return std::nullopt;}
+        std::unordered_set<Bucket*> best_buckets = this->cache.begin()->second;
+
+        Pair best = (*best_buckets.begin())->best;
+        if (best.distance != std::numeric_limits<uint64_t>::max()) {return std::make_pair(best.a, best.b);}
+        if (best.distance == std::numeric_limits<uint64_t>::max() && this->resolution == 0u) {return std::nullopt;}
+
+        std::vector<std::array<uint16_t, 3>> points = this->get_all_points();
+        for (const auto& pair : this->grid) {delete pair.second;}
+        this->cache.clear();
+        this->grid.clear();
+
+        this->resolution = this->resolution - 1u;
+        this->grid_size = std::numeric_limits<uint16_t>::max() / (1u << this->resolution);
+        for (std::array<uint16_t, 3> point : points) {this->add(point);}
+        return this->get_nearest();
+
+    }
+
+};
+
+class AgglomerativeHistogram {
+
+private:
+
+    struct ArrayHash {
+        std::size_t operator()(const std::array<uint16_t, 3>& arr) const noexcept {
+            size_t seed = 0;
+            for (auto elem : arr) {seed ^= std::hash<uint16_t>{}(elem) + 0x9e3779b9 + (seed << 6) + (seed >> 2);}
+            return seed;
+        }
+    };
+
+    std::unordered_map<std::array<uint16_t, 3>, uint32_t, ArrayHash> histogram;
+
+public: 
+
+    bool count(std::array<uint16_t, 3> color) {
+        auto it = this->histogram.find(color);
+        if (it != this->histogram.end()) {it->second++; return false;}
+        this->histogram[color] = 1u;
+        return true;
+    }
+
+    std::array<uint16_t, 3> merge(std::array<uint16_t, 3> a, std::array<uint16_t, 3> b) {
+
+        uint32_t a_count = this->histogram[a];
+        uint32_t b_count = this->histogram[b];
+        uint32_t merged_count = a_count + b_count;
+        double a_ratio = (double) a_count / (double) merged_count;
+        double b_ratio = (double) b_count / (double) merged_count;
+
+        std::array<uint16_t, 3> merged = {
+            static_cast<uint16_t>(a[0] * a_ratio + b[0] * b_ratio),
+            static_cast<uint16_t>(a[1] * a_ratio + b[1] * b_ratio),
+            static_cast<uint16_t>(a[2] * a_ratio + b[2] * b_ratio)
+        };
+
+        this->histogram.erase(a);
+        this->histogram.erase(b);
+
+        auto it = this->histogram.find(merged);
+        if (it != this->histogram.end()) {it->second += merged_count;}
+        this->histogram[merged] = merged_count;
+
+        return merged;
+
+    }
+
+    size_t size() {
+        return this->histogram.size();
+    }
+
+    std::array<uint16_t, 3> last() {
+        if (this->histogram.size() != 1) {throw std::logic_error("AgglomerativeHistogram::last: method must be called when there is exactly one final element remaining.");}
+        return this->histogram.begin()->first;
+    }
+
+};
+
+}
+
+#endif
